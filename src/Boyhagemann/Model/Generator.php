11f00a9b61829fbd14b52a7cf9c619e415044931
= <=>
2013-10-09 18:20:48 +0200

    fix typo

--- src/Boyhagemann/Model/Generator.php
+++ src/Boyhagemann/Model/Generator.php
@@ -101,7 +101,7 @@ class Generator
 		$class = current($file->getClasses());
 		$class->setExtendedClass($builder->getParentClass());
 
-		if(strstr($this->getTable(), '\\')) {
+		if(strstr($builder->getTable(), '\\')) {
 			$class->addUse('Eloquent');
 		}
 
